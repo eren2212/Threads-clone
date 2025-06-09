75a9342d0f650bcc05c5c3b523c9149a97162599
eren iridere <eiridere@gmail.com>
2025-06-09 18:43:43 +0300

    image picker

--- src/app/(protected)/new.tsx
+++ src/app/(protected)/new.tsx
@@ -9,14 +9,19 @@ import {
   Pressable,
   KeyboardAvoidingView,
   Platform,
+  Image,
 } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import Toast from "react-native-toast-message";
 import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { createPost } from "@/services/post";
+import { Ionicons } from "@expo/vector-icons";
+import * as ImagePicker from "expo-image-picker";
 
 export default function NewScreen() {
   const [text, setText] = useState("");
+  const [image, setImage] = useState<string | null>(null);
+
   const { user } = useAuth();
   const queryClient = useQueryClient();
 
@@ -50,6 +55,21 @@ export default function NewScreen() {
     },
   });
 
+  const pickImage = async () => {
+    // No permissions request is necessary for launching the image library
+    let result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ["images"],
+      allowsEditing: true,
+      quality: 1,
+    });
+
+    console.log(result);
+
+    if (!result.canceled) {
+      setImage(result.assets[0].uri);
+    }
+  };
+
   return (
     <SafeAreaView className="p-4 flex-1">
       <KeyboardAvoidingView
@@ -68,6 +88,22 @@ export default function NewScreen() {
           multiline
           numberOfLines={4}
         />
+
+        {image && (
+          <Image
+            source={{ uri: image }}
+            className="w-1/2 aspect-square rounded-lg my-4"
+          />
+        )}
+
+        <View className="flex-row items-center gap-2">
+          <Ionicons
+            onPress={pickImage}
+            name="images-outline"
+            size={24}
+            color={image ? "gray" : "gainsboro"}
+          />
+        </View>
 
         <View className="mt-auto">
           <Pressable
